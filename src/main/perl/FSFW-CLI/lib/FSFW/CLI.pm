b1cbc03db71562f1b75296ab8d3563252097f3cb
Grant McNaught <gmcnaugh@grnoc.iu.edu>
2014-05-09 22:55:43 +0000

    Improved text completion in tab completion and post submission command expansion

--- src/main/perl/FSFW-CLI/lib/FSFW/CLI.pm
+++ src/main/perl/FSFW-CLI/lib/FSFW/CLI.pm
@@ -20,6 +20,7 @@ use GRNOC::Config;
 use FindBin;
 use Data::Dumper;
 use GRNOC::WebService::Client;
+
 #use Text::Table;
 our $VERSION = "1.0.0";
 
@@ -90,58 +91,86 @@ sub expand_commands {
     my $input = shift;
 
     my $new_text;
-
-    my @input_parts   = split( " ", $input );
+    
+    my @input_parts   = split(" ", $input );
+    foreach my $input_part (@input_parts){
+	$input_part = quotemeta($input_part);
+    }
     my $command_list  = $self->get_command_list();
     my $times_matched = 0;
-    my $exact_match ={};
+
+    my $exact_matches = {};
+    my $partial_matches = {};
+    
+
+    #for each command, use exact match for each step avaliable, bomb out if there are multiple partial matches
+    
+
+
     foreach my $command (@$command_list) {
         my $matching_parts = 0;
 
         my @command_parts = split( " ", $command );
         for ( my $i = 0 ; $i < scalar(@input_parts) ; $i++ ) {
-	   
-	    if ($command_parts[$i] =~ /^$input_parts[$i]$/ ) {
-		$exact_match->{$command} +=1;
+	    unless($partial_matches->{$i}){
+		$partial_matches->{$i} = {};
 	    }
-            if ( $command_parts[$i] =~ /^$input_parts[$i].*/ ) {
+	    
+
+	    if ($command_parts[$i] =~ /^$input_parts[$i]$/ ) {
+		$exact_matches->{$i}=$command_parts[$i];
+	    }
+	    elsif ( $command_parts[$i] =~ /^$input_parts[$i].*/ ) {
                 $matching_parts++;
+		$partial_matches->{$i}->{$command_parts[$i]} = 1;
+
+	#print "$command_parts[$i] matches $input_parts[$i]\n";
             }
-
-
 	    
         }
-        if ( $matching_parts == scalar(@command_parts) ) {
-            $new_text = $command;
-            $times_matched++;
-        }
+        
+    }
+    for ( my $i = 0 ; $i < scalar(@input_parts) ; $i++ ) {
+	if ($exact_matches->{$i}){
+	    $new_text .="$exact_matches->{$i} ";
+	}
+	elsif ($partial_matches->{$i} && scalar(keys %{$partial_matches->{$i}}) == 1){
+	    my @values = keys %{$partial_matches->{$i}};
+	    
+	    $new_text .="$values[0] ";
+	}
 	else {
-		#has to match all parts to have any count in exact matches
-		$exact_match->{$command} =0;
+	    #no partial or exact matches, or multiple partial matches
+	    warn "command not found\n";
+	    return(1, $input);
 	}
     }
-    if ( $new_text && $times_matched == 1 ) {
-        return ( 0, $new_text );
-    }
-    elsif ( $times_matched < 1 ) {
-        print "Command not found!\n";
-    }
-    elsif ( $times_matched > 1 ) {
-	my $new_command;
-	my $low_bar=0;
-	foreach my $command (keys %$exact_match){
-	    if ($exact_match->{$command} > $low_bar){
-		$new_command=$command;
-	    }
-	}
-	if($new_command){
-	    warn "expanding $input to $new_command\n";
-	    return (0, $new_command);
-	}
-        print "Command not unique!\n";
-    }
+    chop($new_text);
+    return (0, $new_text);
 
-    return ( 1, $input );
+    # if ( $new_text && $times_matched == 1 ) {
+    #     return ( 0, $new_text );
+    # }
+    # elsif ( $times_matched < 1 ) {
+    #    # print "Command not found!\n";
+    # }
+    # elsif ( $times_matched > 1 ) {
+    # 	my $new_command;
+    # 	my $low_bar=0;
+    # 	foreach my $command (keys %$exact_match){
+    # 	    if ($exact_match->{$command} > $low_bar){
+    # 		warn "found exact match $command";
+    # 		$new_command=$command;
+    # 	    }
+    # 	}
+    # 	if($new_command){
+    # 	    #print "expanding $input to $new_command\n";
+    # 	    return (0, $new_command);
+    # 	}
+    #     print "Command not unique!\n";
+    # }
+
+    # return ( 1, $input );
 }
 
 sub command_complete {
@@ -150,42 +179,119 @@ sub command_complete {
 
     my ( $text, $line, $start ) = @_;
     my $command_list = $self->get_command_list();
-    my @matches      = ();
+    my @matches_at_level;
+   # $line= quotemeta($line);
+    
+    my @text_parts    = split( " ", $line );
+    foreach  (@text_parts){
+	$_ = quotemeta($_);
+    }
+    if ($line eq ""){
+	#warn "Line is empty\n\n";
+	my @return;
+	foreach my $command (@$command_list){
+	    my @command_parts = split( " ", $command );
+	    my $last_word     = $command_parts[0];
+	    push(@return, $command_parts[0]);
+
+	}
+	return @return;
+   }
 
     foreach my $command (@$command_list) {
         my $offset        = 0;
+
         my $is_match      = 1;
-        my @text_parts    = split( " ", $line );
         my @command_parts = split( " ", $command );
         my $last_word     = $command_parts[0];
 
-        for ( my $i = 0 ; $i < scalar(@text_parts) ; $i++ ) {
+	#default to assuming the whole line matches until we find a word that doesn't in our current depth
+	#for n words in line
+	# if all words match and there are no options after it.. this is a full command, woot you've hit the end of the line
+	# if all words match and you have more than one match, your last word options should be all options that match at this depth
+	# if all words match and there are no other matches at this depth, give next depth option
+
+	#warn "number of parts in arry $#text_parts";
+        for ( my $i = 0 ; $i <= $#text_parts; $i++ ) {
+#	    warn "command: $command_parts[$i], text:$text_parts[$i]\n";
+	    unless ($matches_at_level[$i]){
+		$matches_at_level[$i]={};
+	    }
+	    unless ($matches_at_level[$i+1]){
+		$matches_at_level[$i+1]={};
+	    }
+	    
 	    my $is_exact_match=0;
+	    if ( $command_parts[$i] =~ /^$text_parts[$i].*/ ) {
+		#print "matched $command_parts[$i]\n";
+		#$last_word = $command_parts[$i];
+
+		if ($i == $#text_parts){
+		    #print "adding at level $i\n";
+		    #if we've started the next command only accept full matches for additions to the list of matches?
+		    unless ($line =~/.*?\s+$/){
+			#print "index $i, $command_parts[$i] matches $text_parts[$i]";
+			$matches_at_level[$i]->{$command_parts[$i]} =1;
+		    }
+		    
+		}
+	    }
+	    elsif ($is_match && $i == $#text_parts && $text eq ""){
+		#everything matches up to this point, add next option
+		#$matches_at_level[$i]->{$command_parts[$i]} =1;
+		#$matches_at_level[$i+1]->{$command_parts[$i+1]}=1; #=  $command_parts[$i+1];
+	    }
+	    else{
+		if ($i == $#text_parts){
+		   # print "index $i, $command_parts[$i] does not match $text_parts[$i]";
+		}
+		$is_match=0;
+		last;
+	    }
+
 	    if ($command_parts[$i] =~ /^$text_parts[$i]$/ ) {
-		$is_exact_match=1;
-	    }
-            unless ( $command_parts[$i] =~ /^$text_parts[$i].*/ ) {
+		#exactly matches, so add current line and next line to matches at levels
+		if ($i == $#text_parts){
+		   # print "index $i, $command_parts[$i] exactly matches $text_parts[$i]";
+		    $matches_at_level[$i]->{$command_parts[$i]} =1;
+		    $matches_at_level[$i+1]->{$command_parts[$i+1]} =1;
+		}
 
-                $is_match = 0;
-                last;
-            }
-	    unless ($is_exact_match){
-		$last_word = $command_parts[$i];
+		
 	    }
-            if ( length( $text_parts[$i] ) == length( $command_parts[$i] ) ) {
-                $last_word = $command_parts[ $i + 1 ];
-            }
+
 
         }
-        if ($is_match) {
-
-
-            push( @matches, $last_word );
-        }
+	#if ($command =~ /^$line$/){
+	    #command is complete, perfect match don't add any options?
+	#    return;
+	#}
+ 
     }
-
-
-    return @matches;
+    #warn Dumper (\@matches_at_level);
+    #warn scalar(keys %{$matches_at_level[$#text_parts]});
+    if ($matches_at_level[$#text_parts] && (scalar(keys %{$matches_at_level[$#text_parts]}) > 1 )  ){
+	#multiple matches at top level
+	my @return = keys %{$matches_at_level[$#text_parts]};
+#	print "multiple matches at this level $#text_parts : returning ".Dumper(\@return);
+	return @return;
+    }
+    if ($matches_at_level[$#text_parts] && ( scalar(keys %{$matches_at_level[$#text_parts]}) == 1 )){
+	if ($matches_at_level[$#text_parts +1 ] && scalar(keys %{$matches_at_level[$#text_parts +1 ]} )) {
+	    my @return = keys %{$matches_at_level[$#text_parts +1]};
+	   # print "Only one match at current level returning next level: ".Dumper(\@return);
+#return @return;
+	    return @return;
+	    }
+	my @return = keys %{$matches_at_level[$#text_parts]};
+	#print "Only one match at current level and no next level matches yet: ".Dumper(\@return);
+	return @return;
+    }
+    #print "found no matches at $#text_parts \n";
+    return;
+    #my @return = keys (%matches);
+    #print Dumper (\%matches);
+    #return @return;
 }
 
 #stubbed out in case we ever have a legitimate auth system for the WS.
@@ -213,7 +319,7 @@ sub build_command_list {
     
     $ws->set_url("$base_url:$port/fsfw/admin/slices/json"); 
     my $slices_obj = $ws->foo();
-    #warn Dumper ($slices_obj);
+    #print Dumper ($slices_obj);
     my @slices;
     foreach my $slice (keys %$slices_obj){
 	push (@slices, $slice);
@@ -270,7 +376,7 @@ sub handle_input {
     $port='8080';
     ( $insert_text, $input ) = $self->expand_commands($input);  
     
-    if ( $input =~ /exit/ || $input =~ /quit/ ) {
+    if ( $input =~ /^exit$/ || $input =~ /^quit$/ ) {
         exit;
     }
     elsif ( $input =~ /^show switches$/ ) {
@@ -309,9 +415,14 @@ sub handle_input {
 	print Dumper ($slices); #NONPROD
     }
     elsif ( $input =~/^show flows (\S+) (\S+)/){
+	#warn "showing flows.";
 	$ws->set_url("$base_url:$port/fsfw/flows/$1/$2/json"); 
 	    my $flows = $ws->foo();
-	    print Dumper ($flows); #NONPROD
+	    #print Dumper ($flows); #NONPROD
+	
+	if (!defined($flows) || scalar (@$flows) == 0 ){
+	    print "No Flows to display\n";
+	}
 	foreach my $flow (@$flows){
 	    
 	    my $priority = $self->_unsign_int($flow->{'priority'});
@@ -360,7 +471,7 @@ sub handle_input {
 		    }
 		    $action_str .= " $value";
 		}
-		#warn "adding $action_str";
+		#print "adding $action_str";
 		push (@processed_actions, $action_str);
 
 
@@ -371,7 +482,7 @@ sub handle_input {
     elsif ( $input =~/^show status (\S+) (\S+)/){
 	    $ws->set_url("$base_url:$port/fsfw/status/$1/$2/json"); 
 	    my $status_obj = $ws->foo();
-	    print Dumper ($status_obj); #NONPROD
+	    print Dumper ($status_obj); 
     }
 
     return ;#$insert_text;
