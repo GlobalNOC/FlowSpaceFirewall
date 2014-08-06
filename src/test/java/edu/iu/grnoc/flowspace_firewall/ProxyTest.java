3b5314e15911c40eb4c488f155e81b291d4e6984
AJ Ragusa <ajragusa@gmail.com>
2014-08-06 10:56:13 -0400

    added a unit test for wildcarded bypass
    added a unit test to verify that flow rules with the right input
    port/vlan tag but improper wildcards do not get through now

--- src/test/java/edu/iu/grnoc/flowspace_firewall/ProxyTest.java
+++ src/test/java/edu/iu/grnoc/flowspace_firewall/ProxyTest.java
@@ -431,6 +431,8 @@ public class ProxyTest {
 		OFMatch match = new OFMatch();
 		match.setDataLayerVirtualLan((short)100);
 		match.setInputPort((short)1);
+		match.setWildcards(match.getWildcardObj().matchOn(Flag.DL_VLAN));
+		match.setWildcards(match.getWildcardObj().matchOn(Flag.IN_PORT));
 		List<OFAction> actions = new ArrayList<OFAction>();
 				
 		//create the output action
@@ -507,7 +509,8 @@ public class ProxyTest {
 		assertTrue("1 message went to the controller", messagesSentToController.size() == 1);
 		OFMessage msg = messagesSentToController.get(0);
 		assertTrue("Message to Controller was an error", msg.getType().getTypeValue() == OFMessageType.ERROR.getValue());
-		
+		OFError error = (OFError)msg;
+		assertTrue("Error type is now OFPET_FLOW_MOD_FAILED", error.getErrorType() == OFError.OFErrorType.OFPET_FLOW_MOD_FAILED.getValue());
 	}
 	
 	
@@ -797,6 +800,51 @@ public class ProxyTest {
 	}
 	
 	@Test
+	public void testWildCardErrorReturned(){
+		setupSlicer();
+		messagesSentToSwitch.clear();
+		messagesSentToController.clear();
+		Proxy proxy = new Proxy(sw, slicer, fsfw);
+		expect(channel.isConnected()).andReturn(true).anyTimes();
+		expect(handler.isHandshakeComplete()).andReturn(true).anyTimes();
+		EasyMock.replay(handler);
+		EasyMock.replay(channel);
+		assertNotNull("Proxy was created",proxy);
+		assertFalse("Proxy is not connected as expected", proxy.connected());
+		proxy.connect(channel);
+		assertTrue("Proxy is now connected", proxy.connected());
+		
+		//build the match
+		OFMatch match = new OFMatch();
+		match.setDataLayerVirtualLan((short)100);
+		match.setInputPort((short)1);
+		match.setWildcards(match.getWildcardObj().matchOn(Flag.IN_PORT));
+		List<OFAction> actions = new ArrayList<OFAction>();
+				
+		//create the output action
+		OFActionOutput act2 = new OFActionOutput();
+		act2.setPort((short)1);
+		act2.setType(OFActionType.OUTPUT);
+		
+		//add the actions to the action list
+		actions.add(act2);
+				
+		//build the flow
+		OFFlowMod flow = new OFFlowMod();
+		flow.setCommand(OFFlowMod.OFPFC_ADD);
+		flow.setXid(101);
+		flow.setMatch(match);
+		flow.setActions(actions);
+		flow.setLengthU(80);
+		proxy.toSwitch(flow, cntx);
+		
+		log.debug(messagesSentToSwitch.toString());
+		assertTrue("Message was sent to Switch", messagesSentToSwitch.size() == 0);
+		assertTrue("Message was not sent to Controller", messagesSentToController.size() == 1);
+		
+	}
+	
+	@Test
 	public void testErrorReturnedNotPartOfSlice(){
 		setupSlicer();
 		messagesSentToSwitch.clear();
@@ -863,9 +911,6 @@ public class ProxyTest {
 		assertFalse("Proxy is not connected as expected", proxy.connected());
 		proxy.connect(channel);
 		assertTrue("Proxy is now connected", proxy.connected());
-		
-		
-		
 	}
 	
 	@Test
